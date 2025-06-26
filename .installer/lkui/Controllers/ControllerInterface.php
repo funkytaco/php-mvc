74f4fd3338fffda249c3e965552f235213665ced
Luis Gonzalez <weehoo@gmail.com>
2025-06-26 16:38:02 -0400

    WIP: broken routes. updated interface for lkui

--- .installer/lkui/Controllers/ControllerInterface.php
+++ .installer/lkui/Controllers/ControllerInterface.php
@@ -1,9 +1,4 @@
 <?php
-namespace App\Controllers;
-
-use \Main\Router\RouteCollector;
-
 Interface ControllerInterface {
-    public function get(RouteCollector $router);
-    public function post(RouteCollector $router);
+    public function get();
 }
